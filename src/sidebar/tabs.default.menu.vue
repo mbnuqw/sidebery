fc2d6b75bfa524823b06d6f9b6b4bc4c2d7c7f70
mbnuqw <maxbadryzlov@gmail.com>
2018-09-24 12:48:45 +0500

    TabsMenu: Close tabs duplicates

--- src/sidebar/tabs.default.menu.vue
+++ src/sidebar/tabs.default.menu.vue
@@ -9,6 +9,7 @@
       .opt.-false {{t('settings.opt_false')}}
 
   .options
+    .opt(v-if="haveTabs", @click="dedupTabs") {{t('tabs_menu.dedup_tabs')}}
     .opt(v-if="haveTabs", @click="reloadAllTabs") {{t('tabs_menu.reload_all_tabs')}}
     .opt(v-if="haveTabs", @click="closeAllTabs") {{t('default_tabs_menu.close_all_tabs')}}
 </template>
@@ -44,6 +45,18 @@ export default {
       else this.$root.syncPanels.push(id)
       this.$root.resyncPanels()
       this.$root.saveState()
+    },
+
+    dedupTabs() {
+      if (!this.conf.tabs || this.conf.tabs.length === 0) return
+      const toClose = []
+      this.conf.tabs.map((t, i) => {
+        for (let j = i + 1; j < this.conf.tabs.length; j++) {
+          if (this.conf.tabs[j].url === t.url) toClose.push(this.conf.tabs[j].id)
+        }
+      })
+      browser.tabs.remove(toClose)
+      this.$emit('close')
     },
 
     reloadAllTabs() {
