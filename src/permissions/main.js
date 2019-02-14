3d2d8f0ae4f6ecc73a95de518fab8cbaaa14b2e6
mbnuqw <maxbadryzlov@gmail.com>
2019-02-14 12:45:28 +0500

    Fixed: Group page styles

---
+++ src/permissions/main.js
@@ -0,0 +1,34 @@
+// Translation
+void (async function() {
+  document.title = browser.i18n.getMessage('permissions.all_urls.title')
+  const logoEl = document.getElementById('logo')
+  if (logoEl) logoEl.innerText = browser.i18n.getMessage('permissions.all_urls.logo')
+  const msgEl = document.getElementById('msg')
+  if (msgEl) msgEl.innerText = browser.i18n.getMessage('permissions.all_urls.msg')
+  const btnEl = document.getElementById('req_btn')
+  if (btnEl) {
+    const permName = btnEl.getAttribute('data-perm')
+    btnEl.innerText = browser.i18n.getMessage(`permissions.${permName}.btn`)
+  }
+  const noteEl = document.getElementById('note')
+  if (noteEl) noteEl.innerText = browser.i18n.getMessage('permissions.all_urls.note')
+})()
+
+// Permission requesting
+void (async function() {
+  const reqBtnEl = document.getElementById('req_btn')
+  if (!reqBtnEl) return
+
+  // Get permissions
+  const origins = []
+  const permissions = []
+  let perm = reqBtnEl.getAttribute('data-perm')
+  if (perm === 'all_urls') origins.push('<all_urls>')
+  else if (perm) permissions.push(perm)
+
+  reqBtnEl.addEventListener('click', () => {
+    browser.permissions.request({ origins, permissions }).then(() => {
+      browser.runtime.reload()
+    })
+  })
+})()
