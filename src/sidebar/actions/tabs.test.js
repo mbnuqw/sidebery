1d918a1451837c69dc58846465ddf5b140a123a9
mbnuqw <maxbadryzlov@gmail.com>
2019-03-11 21:12:56 +0500

    Fixed: tests

--- src/sidebar/actions/tabs.test.js
+++ src/sidebar/actions/tabs.test.js
@@ -917,28 +917,6 @@ describe('foldTabsBranch', () => {
     expect(dispatch).toBeCalledWith('saveTabsTree')
   })
 
-  test('fold tabs branch with auto expanding', async () => {
-    const state = {
-      hideFoldedTabs: false,
-      autoExpandTabs: true,
-      tabs: [
-        { id: 1, isParent: true, folded: false },
-        { id: 2, parentId: 1, invisible: false, active: true },
-      ],
-    }
-    const dispatch = jest.fn()
-    browser.tabs.update = jest.fn()
-    browser.tabs.hide = jest.fn()
-
-    await TabsActions.foldTabsBranch({ state, dispatch }, 1)
-    await new Promise(r => setTimeout(r, 500))
-    expect(browser.tabs.update).not.toBeCalled()
-    expect(browser.tabs.hide).not.toBeCalled()
-    expect(state.tabs[0].folded).toBe(true)
-    expect(state.tabs[1].invisible).toBe(true)
-    expect(dispatch).toBeCalledWith('saveTabsTree')
-  })
-
   test('fold tabs branch with auto hidding', async () => {
     const state = {
       hideFoldedTabs: true,
