865c24db3197ee1f23c6df668e78c2440c4c778b
Alexandria K. Vail <alexandria.vail@gmail.com>
2020-02-21 16:38:46 -0500

    Fixed: Text correction: parallelism
    The different items on the list were written in different ways. It's mildly arguable which format to match in the case of the permissions list since each item was originally written in a different format, but it's a minor choice in the end, and the consistency improves understandability.

--- addon/locales/en.settings.js
+++ addon/locales/en.settings.js
@@ -331,20 +331,20 @@ export default {
 
   // --- Permissions
   'settings.permissions_title': { message: 'Permissions' },
-  'settings.all_urls_label': { message: 'Websites data:' },
+  'settings.all_urls_label': { message: 'Accessing websites data:' },
   'settings.all_urls_info': {
     message:
       'Required for:\n- Cleaning cookies\n- Proxy and url rules of containers\n- Screenshots for the group page and windows selection panel\n- Changing the User-Agent per container',
   },
   'settings.tab_hide_label': { message: 'Hiding tabs:' },
   'settings.tab_hide_info': {
-    message: 'Required for:\n- Hiding tabs on inactive panels\n- Hiding folded tabs',
+    message: 'Required for:\n- Hiding tabs in inactive panels\n- Hiding folded tabs',
   },
-  'settings.clipboard_write_label': { message: 'Clipboard write:' },
+  'settings.clipboard_write_label': { message: 'Writing to clipboard:' },
   'settings.clipboard_write_info': {
     message: 'Required for:\n- Copying urls of tabs/bookmarks through context menu',
   },
-  'settings.web_request_blocking_label': { message: 'Web requests blocking' },
+  'settings.web_request_blocking_label': { message: 'Blocking web requests:' },
   'settings.web_request_blocking_info': {
     message: 'Required for:\n- Changing the User-Agent per container',
   },
