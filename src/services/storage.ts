90a80194cf0fbed51dde0db7d8108d7437e54343
mbnuqw <maxbadryzlov@gmail.com>
2022-08-01 19:05:56 +0500

    Perf: Storage: Simplified change handling

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,149 +1,112 @@
-import { StorageChanges, Stored, StoredSync, InstanceType, Entries } from 'src/types'
+import { Stored, StoredSync, Entries, IPCNodeInfo } from 'src/types'
 import { Info } from './info'
 import { IPC } from './ipc'
 import { Logs } from './logs'
 import { Settings } from './settings'
 import { Windows } from './windows'
 
-type ChangeHandler = <K extends keyof Stored>(newVal: Stored[K], oldVal: Stored[K]) => void
-type ChangeHandlerG<K extends StorageKey> = (
-  newVal: StorageValue<K>,
-  oldVal: StorageValue<K>
-) => void
+type ChangeHandler = <K extends keyof Stored>(newVal: Stored[K]) => void
+type ChangeHandlerG<K extends StorageKey> = (newVal: StorageValue<K>) => void
 type StorageKey = keyof Stored
 type StorageValue<K extends keyof Stored> = Stored[K]
+type StorageEntries = (Iterator<Stored> &
+  { [key in StorageKey]: [StorageKey, StorageValue<key>] }[StorageKey])[]
 
 export const Store = {
-  setupStorageListeners,
-  resetStorageListeners,
   set,
+  setFromRemoteFg,
   sync,
+  storageChangeListener,
   onKeyChange,
-  offKeyChange,
-  ipcKeyChanged,
-  registerRemote,
-  unregisterRemote,
-  unregisterAllRemote,
 }
 
-function ipcKeyChanged<K extends StorageKey>(
-  key: K,
-  newValue: StorageValue<K>,
-  oldValue: StorageValue<K>
-): void {
-  if (blockedKeys[key]) {
-    unblockKey(key)
-    return
-  }
-  const handler = changeHandlers[key]
-  if (handler) {
-    handler(newValue, oldValue)
-  } else {
-    Logs.warn(`Store: Received store key change event for ${key} without a handler`)
+const SIDEBAR_LISTENED_KEYS = ['settings', 'sidebarCSS', 'sidebar', 'contextMenu', 'containers']
+const SETUP_LISTENED_KEYS = [
+  'settings',
+  'sidebarCSS',
+  'groupCSS',
+  'sidebar',
+  'contextMenu',
+  'containers',
+  'snapshots',
+  'keybindings',
+]
+
+function storageChangeListener(newValues: Stored): void {
+  for (const [key, newValue] of Object.entries(newValues) as Entries<Stored>) {
+    const handler = changeHandlers[key]
+    if (handler) handler(newValue)
   }
 }
 
-function onStorageChange(changes: StorageChanges, type: browser.storage.AreaName): void {
-  if (type !== 'local') return
+let storageBuf: Stored = {}
+let storageBufTimeout: number | undefined
+async function _set(newValues: Stored, srcInfo?: IPCNodeInfo): Promise<void> {
+  if (Info.isBg) {
+    let changesForSidebar: Record<string, any> | undefined
+    let changesForSetup: Record<string, any> | undefined
 
-  for (const [key, change] of Object.entries(changes) as Entries<StorageChanges>) {
-    const remotes = remoteHandlers[key]
-    if (remotes && change) {
-      for (const remoteKey of remotes) {
-        const { type, winId } = JSON.parse(remoteKey) as { type: InstanceType; winId: ID }
-        if (type == InstanceType.sidebar) {
-          IPC.sidebar(winId, 'storeKeyChanged', key, change.newValue, change.oldValue)
-        }
+    for (const [key, newValue] of Object.entries(newValues) as StorageEntries) {
+      if (SIDEBAR_LISTENED_KEYS.includes(key)) {
+        if (!changesForSidebar) changesForSidebar = { [key]: newValue }
+        else changesForSidebar[key] = newValue
+      }
+
+      if (SETUP_LISTENED_KEYS.includes(key)) {
+        if (!changesForSetup) changesForSetup = { [key]: newValue }
+        else changesForSetup[key] = newValue
+      }
+
+      // Call local handler
+      const handler = changeHandlers[key]
+      if (handler && newValue) handler(newValue)
+    }
+
+    // Send changes to all connected sidebars
+    if (changesForSidebar) {
+      for (const con of Object.values(IPC.sidebarConnections)) {
+        if (srcInfo && srcInfo.type === con.type && srcInfo.winId === con.id) return
+        IPC.sidebar(con.id, 'storageChanged', changesForSidebar)
       }
     }
-    if (blockedKeys[key]) {
-      unblockKey(key)
-      continue
+
+    // Send changes to all connected setup pages
+    if (changesForSetup) {
+      for (const con of Object.values(IPC.setupPageConnections)) {
+        if (srcInfo && srcInfo.type === con.type && srcInfo.tabId === con.id) return
+        IPC.setupPage(con.id, 'storageChanged', changesForSetup)
+      }
     }
 
-    const handler = changeHandlers[key]
-    if (handler && change) handler(change.newValue, change.oldValue)
+    // Set new values
+    return browser.storage.local.set<Stored>(newValues)
   }
-}
 
-function setupStorageListeners(): void {
-  browser.storage.onChanged.addListener(onStorageChange)
-}
-
-function resetStorageListeners(): void {
-  browser.storage.onChanged.removeListener(onStorageChange)
-}
-
-const blockedKeys: { [key in keyof Stored]?: number } = {}
-function blockChangeHandling(key: keyof Stored): void {
-  if (blockedKeys[key]) clearTimeout(blockedKeys[key])
-  blockedKeys[key] = setTimeout(() => {
-    Logs.warn(`Store: Did not receive blocked change for ${key} before timing out`)
-    blockedKeys[key] = undefined
-  }, 1200)
-}
-function unblockKey(key: keyof Stored): void {
-  clearTimeout(blockedKeys[key])
-  blockedKeys[key] = undefined
-}
-
-let writingTimeout: number | undefined
-let storageBuf: Stored = {}
-async function _set(newValues: Stored): Promise<void> {
-  for (const key of Object.keys(newValues) as [keyof Stored]) {
-    blockChangeHandling(key)
+  // Or send them to background
+  else {
+    const srcInfo = { type: Info.instanceType, winId: Windows.id, tabId: Info.currentTabId }
+    IPC.bg('saveInLocalStorage', newValues, srcInfo)
   }
-  return browser.storage.local.set<Stored>(newValues)
 }
 async function set(newValues: Stored, delay?: number): Promise<void> {
   if (!delay) return _set(newValues)
+
   storageBuf = { ...storageBuf, ...newValues }
 
-  clearTimeout(writingTimeout)
-  writingTimeout = setTimeout(() => {
+  clearTimeout(storageBufTimeout)
+  storageBufTimeout = setTimeout(() => {
     _set(storageBuf)
     storageBuf = {}
   }, delay)
 }
+function setFromRemoteFg(newValues: Stored, srcInfo: IPCNodeInfo): void {
+  _set(newValues, srcInfo)
+}
 
 const changeHandlers: { [key in keyof Stored]?: ChangeHandler } = {}
 function onKeyChange<K extends keyof Stored, H extends ChangeHandlerG<K>>(key: K, cb: H): void {
-  if (changeHandlers[key]) throw Logs.err(`Storage: onKeyChange: "${key}" handler existed`)
+  if (changeHandlers[key]) throw Logs.err(`Storage: onKeyChange: "${key}" handler already exists`)
   changeHandlers[key] = cb as ChangeHandler
-  // FIXME: Should test !Info.isBg, but currently only sidebars are wired together for IPC to work
-  if (Info.isSidebar) {
-    IPC.bg('registerStoreKeyChange', key, Info.instanceType, Windows.id)
-  }
-}
-function offKeyChange<K extends keyof Stored>(key: K, cb: ChangeHandlerG<K>): void {
-  if (changeHandlers[key] === cb) delete changeHandlers[key]
-  if (Info.isSidebar) {
-    IPC.bg('unregisterStoreKeyChange', key, Info.instanceType, Windows.id)
-  }
-}
-
-const remoteHandlers: { [key in keyof Stored]?: Set<string> } = {}
-function registerRemote(key: StorageKey, type: InstanceType, winId: ID): void {
-  const remoteKey = JSON.stringify({ type, winId })
-  const handlers = remoteHandlers[key] ?? new Set()
-  if (handlers.has(remoteKey))
-    Logs.warn(`Storage: registerRemote: "${key}" handler already exists for ${remoteKey}`)
-  handlers.add(remoteKey)
-  remoteHandlers[key] = handlers
-}
-function unregisterRemote(key: StorageKey, type: InstanceType, winId: ID): void {
-  const remoteKey = JSON.stringify({ type, winId })
-  const handlers = remoteHandlers[key]
-  if (!handlers) return
-  handlers.delete(remoteKey)
-  remoteHandlers[key] = handlers
-}
-function unregisterAllRemote(type: InstanceType, winId: ID): void {
-  const remoteKey = JSON.stringify({ type, winId })
-  for (const key of Object.keys(remoteHandlers)) {
-    const handlers = remoteHandlers[key as keyof Stored]
-    if (handlers) handlers.delete(remoteKey)
-  }
 }
 
 export async function sync(name: string, value: Stored): Promise<void> {
