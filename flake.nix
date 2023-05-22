eab12dc8deebe80f676a4ceb0036eb658851a948
gotjoshua <gotjoshua@users.noreply.github.com>
2023-05-22 16:51:59 +0100

    [devenv] initial setup (#1044)
    Co-authored-by: Manu [tennox] <tennox+git@txlab.io>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  # Basically boilerplate - generated from: https://github.com/cachix/devenv/tree/main/templates
+  # ! not maintained by @mbnuqw - report issues here: https://github.com/onezoomin/sidebery/issues
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://devenv.cachix.org" ];
+    extra-trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              (import ./devenv.nix { inherit pkgs inputs; })
+            ];
+          };
+        });
+    };
+}
