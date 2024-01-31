e8737465c4498e97f0c02100160709fc859d22b2
mbnuqw <maxbadryzlov@gmail.com>
2024-01-31 21:43:09 +0500

    fix: remove unused code (progress-bar) from ScrollBox

--- src/components/scroll-box.vue
+++ src/components/scroll-box.vue
@@ -1,15 +1,14 @@
 <template lang="pug">
 .ScrollBox(ref="el" @wheel="onWheel")
-  //- .progress(ref="progressEl" :data-scrolling="state.scrolling")
   .top-shadow(:data-show="state.topOverflow")
   .bottom-shadow(:data-show="state.bottomOverflow")
-  .scroll-container(ref="scrollBoxEl" tabindex="-1" @scroll.passive="recalcScroll(true, true)")
+  .scroll-container(ref="scrollBoxEl" tabindex="-1" @scroll.passive="recalcScroll(true)")
     .scrollable(ref="scrollContentEl")
       slot
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref, onMounted, onBeforeUnmount, nextTick } from 'vue'
+import { reactive, ref, onMounted, onBeforeUnmount } from 'vue'
 import { ScrollBoxComponent } from 'src/types'
 import * as Utils from 'src/utils'
 import * as Logs from 'src/services/logs'
@@ -24,14 +23,12 @@ const props = defineProps<{
 }>()
 
 const el = ref<HTMLElement | null>(null)
-// const progressEl = ref<HTMLElement | null>(null)
 const scrollBoxEl = ref<HTMLElement | null>(null)
 const scrollContentEl = ref<HTMLElement | null>(null)
 
 const state = reactive({
   topOverflow: false,
   bottomOverflow: false,
-  scrolling: false,
 })
 
 let boxHeight = 0
@@ -64,9 +61,7 @@ function onWheel(e: WheelEvent): void {
   if (props.lock) e.preventDefault()
 }
 
-let hideProgress = false
-let scrollingStartEndTimeout: number | undefined
-function recalcScroll(progressBar?: boolean, bottomEvent?: boolean): void {
+function recalcScroll(bottomEvent?: boolean): void {
   if (!scrollBoxEl.value || !scrollContentEl.value) return
   boxHeight = scrollBoxEl.value.offsetHeight
   contentHeight = scrollContentEl.value.offsetHeight
@@ -82,29 +77,11 @@ function recalcScroll(progressBar?: boolean, bottomEvent?: boolean): void {
     state.bottomOverflow = false
     if (bottomEvent) emit('bottom')
   }
-  if (contentHeight <= boxHeight) return
-
-  // let scrollGripY = (contentY / (contentHeight - boxHeight)) * 100
-  // if (progressEl.value) {
-  //   if (contentY === 0) scrollGripY = 0
-  //   if (scrollGripY > 100) scrollGripY = 100
-  //   progressEl.value.style.transform = `translateX(${scrollGripY}%)`
-  // }
-
-  if (progressBar && !hideProgress) {
-    if (!scrollingStartEndTimeout) state.scrolling = true
-    scrollingStartEndTimeout = Utils.wait(scrollingStartEndTimeout, 500, () => {
-      state.scrolling = false
-      scrollingStartEndTimeout = undefined
-    })
-  }
-  if (hideProgress) hideProgress = false
 }
 
 function setScrollY(y: number): void {
   if (!scrollBoxEl.value) return
   scrollBoxEl.value.scrollTop = y
-  hideProgress = true
 }
 
 function getScrollBox(): HTMLElement | null {
