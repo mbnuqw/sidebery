33732316206f2bbf8ad966a5b09fa56a55697dcc
the-nelsonator <thenelsonator01@gmail.com>
2026-05-28 19:45:50 -0700

    2129: cleanup, fix pinned tabs, inactivating when none eligible

--- src/services/menu.fg.options.tabs.ts
+++ src/services/menu.fg.options.tabs.ts
@@ -310,27 +310,37 @@ export const tabsMenuOptions: Record<string, () => MenuOption | MenuOption[] | u
   },
 
   discardOtherTabs: () => {
+    const getEligibleTabIds = (): ID[] => {
+      const tabIds: ID[] = []
+      const ids = Selection.ids()
+      const firstTab = Tabs.byId[ids[0]]
+      if (firstTab) {
+        const panel = Sidebar.panelsById[firstTab.panelId]
+        if (Utils.isTabsPanel(panel)) {
+          panel.tabs.forEach(t => !ids.includes(t.id) && tabIds.push(t.id))
+          if (!Settings.state.pinnedNoUnload || !Settings.state.pinnedNoUnloadExplicit) {
+            panel.pinnedTabs.forEach(t => !ids.includes(t.id) && tabIds.push(t.id))
+          }
+        }
+      }
+      return tabIds
+    }
+
     const option: MenuOption = {
       label: translate('menu.tab.discard_other'),
       icon: 'icon_discard',
       onClick: () => {
-        const ids = Selection.ids()
-        const firstTab = Tabs.byId[ids[0]]
-        if (!firstTab) return
-        const panel = Sidebar.panelsById[firstTab.panelId]
-        if (!Utils.isTabsPanel(panel)) return
-        const tabIds: ID[] = []
-        panel.tabs.forEach(t => !ids.includes(t.id) && tabIds.push(t.id))
-        if (tabIds.length) Tabs.discardTabs(tabIds)
+        const tabIds: ID[] = getEligibleTabIds()
+        if (tabIds.length) Tabs.discardTabs(tabIds, true)
       },
     }
 
     const tabId = Selection.getFirst()
     const tab = Tabs.byId[tabId]
-    if (!tab || tab.pinned) option.inactive = true
-    if (tab) {
+    if (!tab) option.inactive = true
+    else {
       const panel = Sidebar.panelsById[tab.panelId]
-      if (!panel || panel.reactive.len === 1) option.inactive = true
+      if (!panel || getEligibleTabIds().length === 0) option.inactive = true
     }
     if (!Settings.state.ctxMenuRenderInact && option.inactive) return
     return option
