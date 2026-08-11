fb0b767f87679b5b08d56893948429003bbe96f6
mbnuqw <maxbadryzlov@gmail.com>
2026-08-11 17:02:02 +0500

    chore: publicSuffix API types

--- src/types/web-ext.d.ts
+++ src/types/web-ext.d.ts
@@ -1453,4 +1453,17 @@ declare namespace browser {
     function getRedirectURL(): string
     function launchWebAuthFlow(details: LaunchWebAuthDetails): Promise<string>
   }
+
+  namespace publicSuffix {
+    type DomainEncoding = 'punycode' | 'display'
+    interface GetDomainOptions {
+      encoding?: DomainEncoding
+      allowIPAddress?: boolean
+      allowPlainSuffix?: boolean
+      allowUnknownSuffix?: boolean
+    }
+    function getDomain(hostname: string, options?: GetDomainOptions): string
+    function getKnownSuffix(hostname: string): string
+    function isKnownSuffix(hostname: string): boolean
+  }
 }
