622891943b4ace519b827bf67eed9da07b8b6f4b
mbnuqw <maxbadryzlov@gmail.com>
2026-09-10 20:07:29 +0500

    chore: rename .AnimatedTabList to .TabList

--- src/sidebar/components/tab-list.vue
+++ src/sidebar/components/tab-list.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.AnimatedTabList(ref="rootEl" @mouseleave="onMouseLeave")
+.TabList(ref="rootEl" @mouseleave="onMouseLeave")
   TabComponent(v-for="id in panel.reactive.visibleTabIds" :key="id" :tabId="id")
   NewTabBar(v-if="Settings.newTabBarPositionAfterTabs" :panel="panel")
   .scroll-retainer(ref="srEl")
